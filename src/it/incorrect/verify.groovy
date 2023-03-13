04f588aba2d4f4aaa2a52caf1d0929d53649b11d
Ivan <clicker.heroes.acg@gmail.com>
2023-03-13 19:57:11 +0300

    assertion

--- src/it/incorrect/verify.groovy
+++ src/it/incorrect/verify.groovy
@@ -1,3 +1,5 @@
 String log = new File(basedir, 'build.log').text;
-assert !log.contains("Violations not found")
+[
+  "Method 'HasSetters#setField' has wrong method signature, because method body contains an assignment, setters violates OOP principles"
+].each { assert log.contains(it): "Log doesn't contain ['$it']" }
 true
