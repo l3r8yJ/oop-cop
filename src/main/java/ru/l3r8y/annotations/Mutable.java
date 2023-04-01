8b842dd48d7efecafc22792622ed0a44cddc0d96
Ivan <clicker.heroes.acg@gmail.com>
2023-04-02 00:40:43 +0300

    imports
    Signed-off-by: Ivan <clicker.heroes.acg@gmail.com>

--- src/main/java/ru/l3r8y/annotations/Mutable.java
+++ src/main/java/ru/l3r8y/annotations/Mutable.java
@@ -24,7 +24,11 @@
 
 package ru.l3r8y.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Mark annotation for mutable classes.
